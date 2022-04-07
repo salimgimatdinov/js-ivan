e0a65c6efa86f32b1ac25618f500f573c6a3525a
salimgimatdinov <salimgimatdinov@yandex.ru>
2022-04-06 22:16:51 -0500

    lesson 031

--- part_02/js/script.js
+++ part_02/js/script.js
@@ -1,2 +1,28 @@
 "use strict"
 
+const btn = document.querySelector('button')
+
+// btn.onclick = function() {
+
+// }
+
+// // mouseenter, 
+// btn.addEventListener('click', (event) => {
+//     console.log(event.target);
+//     event.target.remove()
+//     // alert('cliked')
+// })
+
+let i = 0
+const deleteElement = (event) => {
+    event.target.remove()
+    i++
+    if(i == 1) {
+        btn.removeEventListener('click', deleteElement)
+    }
+}
+btn.addEventListener('click', deleteElement)
+
+
+// event.preventDefault()
+// btn.addEventListener('click', deleteElement, {once: true})
